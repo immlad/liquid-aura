5270141e0c39e70983203311b15467d1a8753157
immlad <corniestc@proton.me>
2026-05-03 14:48:01 -0700

    asfasf

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { JasonSyncContext } from "./contexts/JasonSyncContext";
-
-// Read sync params from JASON OS
-const params = new URLSearchParams(window.location.search);
+const hash = window.location.hash;
+const queryString = hash.includes("?") ? hash.split("?")[1] : "";
+const params = new URLSearchParams(queryString);
 
 const syncedUser = params.get("user");
 const syncedAdmin = params.get("admin") === "true";
 const syncedTheme = params.get("theme");
 const syncedAvatar = params.get("avatar");
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <JasonSyncContext.Provider
-      value={{
-        user: syncedUser,
-        admin: syncedAdmin,
-        theme: syncedTheme,
-        avatar: syncedAvatar
-      }}
-    >
-      <App />
-    </JasonSyncContext.Provider>
-  </React.StrictMode>
-);
